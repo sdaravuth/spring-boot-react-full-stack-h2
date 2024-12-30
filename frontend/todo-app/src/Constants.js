4748dcb0563006cc2012a87ae086b99a7cfb73ea
daravuth <daravuth.sd@gmail.com>
2024-12-30 14:25:15 +0700

    Update Constants.js

--- frontend/todo-app/src/Constants.js
+++ frontend/todo-app/src/Constants.js
@@ -1,5 +1,5 @@
 /* For Best Practices https://facebook.github.io/create-react-app/docs/adding-custom-environment-variables*/
-export const API_URL = 'http://165.232.171.100:8083'
+export const API_URL = 'https://165.232.171.100:8084'
 //export const API_URL = 'http://rest-api-full-stack-ranga-101.cfapps.io'
 //export const API_URL = 'https://restful-web-services-full-stack-bankend.cfapps.io'
 export const JPA_API_URL = `${API_URL}/jpa`
